5a5369fa79560fc4781f59cc5145de3791a1cff3
PaL_RYz <rafiokeokeoke@gmail.com>
2026-07-23 12:52:09 +0700

    Membuat layout utama frontend

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/', [HomeController::class, 'index'])->name('home');
