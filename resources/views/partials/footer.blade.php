e13a777955ffada4c5a9d0c33150b3dfd93b2f23
PaL_RYz <rafiokeokeoke@gmail.com>
2026-07-23 11:40:00 +0700

    Membuat layout utama frontend

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <div class="container">
+        <p class="mb-0">
+            &copy; {{ date('Y') }} UnemployedByAI. All Rights Reserved.
+        </p>
+    </div>
+</footer>
