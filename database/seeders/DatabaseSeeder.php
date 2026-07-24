1f9c06145a5524bbe8417cca00442ac37730bfe5
arbiansayh10-pro <vivobook0110@gmail.com>
2026-07-24 21:43:27 +0700

    Save local changes sebelum merge frontend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+
             AdminSeeder::class,
+            CategorySeeder::class,
+
         ]);
     }
 }
